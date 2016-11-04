efc7cff1ae774522dbd66a82a8edf8d0c0f7f3c3
Evan <evan.zlotnick@kasasa.com>
2016-11-04 10:30:56 -0500

    make arrows a little bit smaller like a baller

--- src/components/trend.js
+++ src/components/trend.js
@@ -3,21 +3,21 @@ exports.trendsToText = function(number) {
         case 0:
             return "None";
         case 1:
-            return '<i class="fa fa-arrow-up fa-3x"/><i class="fa fa-arrow-up fa-3x"/>';
+            return '<i class="fa fa-arrow-up fa-2x"/><i class="fa fa-arrow-up fa-2x"/>';
         case 2:
-            return '<i class="fa fa-arrow-up fa-3x"/>';
+            return '<i class="fa fa-arrow-up fa-2x"/>';
         case 3:
 			//forty five up
-            return '<i class="fa fa-arrow-right fa-rotate-45-up fa-3x"/>';
+            return '<i class="fa fa-arrow-right fa-rotate-45-up fa-2x"/>';
         case 4:
-            return '<i class="fa fa-arrow-right fa-3x"/>';
+            return '<i class="fa fa-arrow-right fa-2x"/>';
         case 5:
 			//45 down
-            return '<i class="fa fa-arrow-right fa-rotate-45-down fa-3x"/>';
+            return '<i class="fa fa-arrow-right fa-rotate-45-down fa-2x"/>';
         case 6:
-            return '<i class="fa fa-arrow-down fa-3x"/>';
+            return '<i class="fa fa-arrow-down fa-2x"/>';
         case 7:
-            return '<i class="fa fa-arrow-down fa-3x"/><i class="fa fa-arrow-down fa-3x"/>';
+            return '<i class="fa fa-arrow-down fa-2x"/><i class="fa fa-arrow-down fa-2x"/>';
         case 8:
             return "Cannot compute";
         case 9:
