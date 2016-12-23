17cd93af59da31bf4d6636fcf09d6f5cb46c8143
Evan Zlotnick <evan_zlotnick@yahoo.com>
2016-12-23 11:57:50 -0600

    changing the about page again cuz im a nit

--- src/components/about.js
+++ src/components/about.js
@@ -10,13 +10,13 @@ class Main extends React.Component {
                 <div className="col-sm-12">
                     <div className="panel panel-default">
                         <div className="panel-heading">
-                            <h3 className="panel-title">DISCLAIMERr</h3>
+                            <h3 className="panel-title">DISCLAIMER</h3>
                         </div>
                         <div className="panel-body">
                             <div className="text-left col-sm-12">
-                                All information, thought, and code described here is intended for informational and educational purposes only.
-                                I currently make no attempt at HIPAA privacy compliance. Use of this code is without warranty or support of any kind.
-                                Use at your own risk, and do not use the information or code to make medical decisions.
+                                All information, thought, and code described here is intended for informational and educational purposes only. <br/>
+                                I currently make no attempt at HIPAA privacy compliance. Use of this code is without warranty or support of any kind. <br/>
+                                Use at your own risk, and do not use the information or code to make medical decisions. <br/>
                             </div>
                         </div>
                     </div>
